fa79597dabd05fbd5d0585556d1b9d5b7a724b0f
Rafael Fadel Silva <rafaelfadel2@gmail.com>
2022-07-21 15:04:07 -0300

    finished the dead_state function, rand generator function and render function

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,55 @@
 use anyhow::{Context, Result};
+use rand::Rng;
 use std::fmt;
 
 fn main() {
-    println!("{:?}", Board::dead_state(5, 5));
+    let board = Board::dead_state(5, 5);
+    let board_random = Board::random_state(board);
+    println!("{:?}", &board_random);
+    Board::render(board_random);
 }
 
 #[derive(Debug)]
 struct Board {
-    width: Vec<Vec<usize>>,
-    heigth: Vec<Vec<usize>>,
+    width: usize,
+    height: usize,
+    board: Vec<Vec<u8>>,
 }
 
 impl Board {
-    fn dead_state(width: usize, heigth: usize) -> Board {
-        let width = vec![vec![0; width]; width];
-        let heigth = vec![vec![0; heigth]; heigth];
-        Board { width, heigth }
+    fn dead_state(height: usize, width: usize) -> Self {
+        let board = vec![vec![0; width]; height];
+        Board {
+            height,
+            width,
+            board,
+        }
+    }
+    fn random_state(mut board: Self) -> Self {
+        for i in 0..board.height {
+            for j in 0..board.width {
+                let random = rand::thread_rng().gen_range(-1, 1);
+                board.board[i][j] = if random == 0 { 1 } else { 0 }
+            }
+        }
+        board
+    }
+    fn render(board: Self) {
+        for i in 0..board.height {
+            let mut counter = 0;
+            for j in 0..board.width {
+                counter += 1;
+                if board.board[i][j] == 1 {
+                    print!(" # ");
+                } else {
+                    print!(" - ");
+                }
+                if counter == board.height {
+                    println!("");
+                    counter = 0;
+                }
+            }
+        }
     }
 }
 
@@ -23,5 +57,3 @@ enum BoardState {
     Alive,
     Dead,
 }
-
-fn random_state() {}
